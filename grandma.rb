c3314b1d802c03fdc04f7012ae4c4e0bded02097
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-23 21:09:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-# Write a speak_to_grandma method.
+
 
 def speak_to_grandma(string)
   if string != string.upcase
@@ -10,14 +10,3 @@ def speak_to_grandma(string)
   end 
 end 
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
