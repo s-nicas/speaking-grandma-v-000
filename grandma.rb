399232aef6509fe7a4050b59dc544fe6a7fdaba4
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-23 21:08:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,8 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.upcase 
   return "NO, NOT SINCE 1938!"
-elsif string == "I LOVE YOU GRANDMA!"
-return 
+else string == "I LOVE YOU GRANDMA!"
+return "I LOVE YOU TOO PUMPKIN!"
 end 
 
 # Whatever you say to grandma, she should respond with
